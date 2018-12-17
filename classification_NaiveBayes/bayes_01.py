8ef2a77af50bf1bba7fa98c912335c40769def6d
VictorDu1990 <410343283@qq.com>
2018-12-17 22:32:29 +0800

    Update bayes_01.py

--- classification_NaiveBayes/bayes_01.py
+++ classification_NaiveBayes/bayes_01.py
@@ -1,3 +1,80 @@
 # -*- coding: UTF-8 -*-
 import numpy as np
 from functools import reduce
+
+
+def loadDataSet():
+	postingList=[['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],				
+				['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
+				['my', 'dalmation', 'is', 'so', 'cute', 'I', 'love', 'him'],
+				['stop', 'posting', 'stupid', 'worthless', 'garbage'],
+				['mr', 'licks', 'ate', 'my', 'steak', 'how', 'to', 'stop', 'him'],
+				['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']]
+	classVec = [0,1,0,1,0,1]   																
+	return postingList,classVec	
+	
+def createVocabList(dataSet):
+	vocabSet = set([])  					
+	for document in dataSet:				
+		vocabSet = vocabSet | set(document) 
+	return list(vocabSet)
+	
+def setOfWords2Vec(vocabList, inputSet):
+	returnVec = [0] * len(vocabList)									
+	for word in inputSet:												
+		if word in vocabList:											
+			returnVec[vocabList.index(word)] = 1
+		else: print("the word: %s is not in my Vocabulary!" % word)
+	return returnVec
+	
+	
+def trainNB0(trainMatrix,trainCategory):
+	numTrainDocs = len(trainMatrix)							
+	numWords = len(trainMatrix[0])							
+	pAbusive = sum(trainCategory)/float(numTrainDocs)		
+	p0Num = np.zeros(numWords); p1Num = np.zeros(numWords)	
+	p0Denom = 0.0; p1Denom = 0.0                        	
+	for i in range(numTrainDocs):
+		if trainCategory[i] == 1:							
+			p1Num += trainMatrix[i]
+			p1Denom += sum(trainMatrix[i])
+		else:												
+			p0Num += trainMatrix[i]
+			p0Denom += sum(trainMatrix[i])
+	p1Vect = p1Num/p1Denom									
+	p0Vect = p0Num/p0Denom          
+	return p0Vect,p1Vect,pAbusive	
+
+def classifyNB(vec2Classify, p0Vec, p1Vec, pClass1):
+	p1 = reduce(lambda x,y:x*y, vec2Classify * p1Vec) * pClass1    			
+	p0 = reduce(lambda x,y:x*y, vec2Classify * p0Vec) * (1.0 - pClass1)
+	print('p0:',p0)
+	print('p1:',p1)
+	if p1 > p0:
+		return 1
+	else: 
+		return 0	
+		
+def testingNB():
+	listOPosts,listClasses = loadDataSet()							
+	myVocabList = createVocabList(listOPosts)						
+	trainMat=[]
+	for postinDoc in listOPosts:
+		trainMat.append(setOfWords2Vec(myVocabList, postinDoc))		
+	p0V,p1V,pAb = trainNB0(np.array(trainMat),np.array(listClasses))
+	testEntry = ['love', 'my', 'dalmation']							
+	thisDoc = np.array(setOfWords2Vec(myVocabList, testEntry))		
+	if classifyNB(thisDoc,p0V,p1V,pAb):
+		print(testEntry,'Belonging to the category of insult')								
+	else:
+		print(testEntry,'NOT Belonging to the category of insult')								
+	testEntry = ['stupid', 'garbage']								
+
+	thisDoc = np.array(setOfWords2Vec(myVocabList, testEntry))		
+	if classifyNB(thisDoc,p0V,p1V,pAb):
+		print(testEntry,'Belonging to the category of insult')								
+	else:
+		print(testEntry,'NOT Belonging to the category of insult')								
+
+if __name__ == '__main__':
+	testingNB()
