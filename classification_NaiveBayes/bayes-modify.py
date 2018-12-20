0f5815cfca12f21acb14b86bb30a7c4dde37d4ec
VictorDu1990 <410343283@qq.com>
2018-12-20 23:28:41 +0800

    Update bayes-modify.py

--- classification_NaiveBayes/bayes-modify.py
+++ classification_NaiveBayes/bayes-modify.py
@@ -11,21 +11,7 @@ def createVocabList(dataSet):
         vocabSet = vocabSet | set(document) 
     return list(vocabSet)
 
-"""
-函数说明:根据vocabList词汇表，将inputSet向量化，向量的每个元素为1或0
 
-Parameters:
-	vocabList - createVocabList返回的列表
-	inputSet - 切分的词条列表
-Returns:
-	returnVec - 文档向量,词集模型
-Author:
-	Jack Cui
-Blog:
-	http://blog.csdn.net/c406495762
-Modify:
-	2017-08-11
-"""
 def setOfWords2Vec(vocabList, inputSet):
     returnVec = [0] * len(vocabList)									
     for word in inputSet:												
@@ -74,3 +60,36 @@ def textParse(bigString):
     listOfTokens = re.split(r'\W*', bigString)                              
     return [tok.lower() for tok in listOfTokens if len(tok) > 2]            
 
+
+def spamTest():
+    docList = []; classList = []; fullText = []
+    for i in range(1, 26):                                                 
+        wordList = textParse(open('email/spam/%d.txt' % i, 'r').read())    
+        docList.append(wordList)
+        fullText.append(wordList)
+        classList.append(1)                                                
+        wordList = textParse(open('email/ham/%d.txt' % i, 'r').read())     
+        docList.append(wordList)
+        fullText.append(wordList)
+        classList.append(0)                                                
+    vocabList = createVocabList(docList)                                   
+    trainingSet = list(range(50)); testSet = []                                             
+    for i in range(10):                                                    
+        randIndex = int(random.uniform(0, len(trainingSet)))               
+        testSet.append(trainingSet[randIndex])                             
+        del(trainingSet[randIndex])                                        
+    trainMat = []; trainClasses = []                                       
+    for docIndex in trainingSet:                                           
+        trainMat.append(setOfWords2Vec(vocabList, docList[docIndex]))      
+        trainClasses.append(classList[docIndex])                           
+    p0V, p1V, pSpam = trainNB0(np.array(trainMat), np.array(trainClasses)) 
+    errorCount = 0                                                         
+    for docIndex in testSet:                                               
+        wordVector = setOfWords2Vec(vocabList, docList[docIndex])           
+        if classifyNB(np.array(wordVector), p0V, p1V, pSpam) != classList[docIndex]:   
+            errorCount += 1                                                 
+            print("ERROR predict testSet：",docList[docIndex])
+    print('error rate：%.2f%%' % (float(errorCount) / len(testSet) * 100))
+
+if __name__ == '__main__':
+    spamTest()
